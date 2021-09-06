79eccb3a7ebab27a9b52977f3ac542d8a01216d8
vadimshut <vadimshut1992@gmail.com>
2021-09-07 00:27:17 +0300

    fix: add formdata to form

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,25 +1,23 @@
 import './button.sass'
 
-
-
 interface IButtonProps {
     value: string
     size: 'large' | 'medium' | 'small'
     theme: 'dark' | 'light'
-    
+    form?: string
 }
-
-
 
 export default function Button(props: IButtonProps): JSX.Element {
     const clasName = `button button__${props.size} button__${props.theme}`
-  
+    const handleClick = () => {console.log('test');
+    } 
     return(
         <input 
             className={clasName}
             type="submit" 
             value={props.value}
-            
+            onClick={handleClick}
+            form={props.form || ''}
         />
     )
 }
--- client/src/components/ConnectToLobby/ConnectToLobby.tsx
+++ client/src/components/ConnectToLobby/ConnectToLobby.tsx
@@ -29,68 +29,85 @@ export default function ConnectToLobby(): JSX.Element {
         setJobPosition(job)
     }
 
-    const handleChangeAva = (src: string) => {
-        setSrcAva(src)
+    const handleChangeAvatar = (src: string) => {
+        setSrcAva(src) 
+    }
+
+    const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const a: HTMLFormElement = event.currentTarget             
+        const formData = new FormData(a)
+        console.log(formData)
     }
 
     return (
         <div className="ctl">
             <div className="ctl__form-wrapper">
-                <p className="ctl__title">Connect to lobby</p>
-                <Switcher 
-                    title='Connect as Observer' 
-                    defaultValue={switchValue} 
-                    onChangeToogle={handleSwitchChange}
-                    className="switcher-observer"
-                />
 
-                <FormInputText 
-                    title="Your first name:"
-                    name="firstName"
-                    initialValue={firstName}
-                    validate
-                    onValueChange={handleChangeFirstName}
-                />
+                <form 
+                    id="ctl-form"
+                    onSubmit={handleSubmit}
+                >
+                    <div className="ctl__form-title-wrapper">
+                        <p className="ctl__title">Connect to lobby</p>
+                        <Switcher 
+                            title='Connect as Observer' 
+                            defaultValue={switchValue} 
+                            onChangeToogle={handleSwitchChange}
+                            className="switcher-observer"
+                        />
 
-                <FormInputText 
-                    title="Your last name (optional):"
-                    name="lastName"
-                    initialValue={lastName}
-                    validate={false}
-                    onValueChange={handleChangeLastName}
-                />
+                    </div>
 
-                <FormInputText 
-                    title="Your job position (optional):"
-                    name="jobPosition"
-                    initialValue={jobPosition}
-                    validate={false}
-                    onValueChange={handleChangeJobPosition}
-                />
+                    <FormInputText 
+                        title="Your first name:"
+                        name="firstName"
+                        initialValue={firstName}
+                        validate
+                        onValueChange={handleChangeFirstName}
+                    />
 
-                <AvatarInput 
-                    title="Image"
-                    name="addAvatar"
-                    onAvaChange={handleChangeAva}
-                />
+                    <FormInputText 
+                        title="Your last name (optional):"
+                        name="lastName"
+                        initialValue={lastName}
+                        validate={false}
+                        onValueChange={handleChangeLastName}
+                    />
 
-                <Avatar 
-                    name={firstName} 
-                    lastName={lastName}
-                    src={srcAva}
-                />
+                    <FormInputText 
+                        title="Your job position (optional):"
+                        name="jobPosition"
+                        initialValue={jobPosition}
+                        validate={false}
+                        onValueChange={handleChangeJobPosition}
+                    />
 
-                <Button 
-                    value="Confirm"
-                    size="medium"
-                    theme="dark"
-                />
+                    <AvatarInput 
+                        title="Image"
+                        name="addAvatar"
+                        onAvaChange={handleChangeAvatar}
+                    />
+                    <Avatar 
+                        name={firstName} 
+                        lastName={lastName}
+                        src={srcAva}
+                    />
+                </form>
+               <div className="ctl__btns-wrapper">
+                    <Button 
+                        value="Confirm"
+                        size="medium"
+                        theme="dark"
+                        form="ctl-form"
+                    />
 
-                <Button 
-                    value="Cancel"
-                    size="medium"
-                    theme="light"
-                />
+                    <Button 
+                        value="Cancel"
+                        size="medium"
+                        theme="light"
+                    />
+               </div>   
             </div>
         </div>
     )
