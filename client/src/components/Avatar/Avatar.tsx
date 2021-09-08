273991a2e8ed009dcf9a0be3c9fe1ff2c8814e7a
Vadim <vadimshut1992@gmail.com>
2021-09-08 09:32:18 +0300

    fix: add size small to avatar component

--- client/src/components/Avatar/Avatar.tsx
+++ client/src/components/Avatar/Avatar.tsx
@@ -4,10 +4,12 @@ interface IAvatarProps {
   name: string
   lastName: string
   src: string
+  size: 'large' | 'small'
 }
 
 export default function Avatar(props: IAvatarProps): JSX.Element {
   const nameFirstLetter = props.name === '' ? 'N' : props.name[0]
+  const className = props.size === 'large' ? 'avatar avatar__large' : 'avatar avatar__small'
   let lastNameFirstLetter = ''
   if (props.lastName === '' && props.name !== '') {
     lastNameFirstLetter = props.name[props.name.length - 1]
@@ -19,14 +21,14 @@ export default function Avatar(props: IAvatarProps): JSX.Element {
 
   if (props.src === '') {
     return (
-      <div className="avatar">
+      <div className={className}>
         {nameFirstLetter}
         {lastNameFirstLetter}
       </div>
     )
   }
   return (
-    <div className="avatar">
+    <div className={className}>
       <img className="avatar__img" src={props.src} alt={props.name} />
     </div>
   )
