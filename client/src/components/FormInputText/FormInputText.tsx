533cc472d891a49ba129f3577dbee42e7758743a
Daniil <uaolmer2@gmail.com>
2021-09-08 19:27:01 +0300

    fix: component names

--- client/src/components/FormInputText/FormInputText.tsx
+++
@@ -1,3 +0,0 @@
-export default function FormInputText(): JSX.Element {
-  return <div />
-}
--- client/src/components/footer/footer.tsx
+++
@@ -1,5 +0,0 @@
-import './Footer.sass'
-
-export default function Footer(): JSX.Element {
-  return <footer />
-}
