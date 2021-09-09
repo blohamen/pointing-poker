d1e397b547eda4851d75dce3c85e53b7939fbbe8
Daniil <uaolmer2@gmail.com>
2021-09-09 20:22:48 +0300

    feat: add footer, improve layout

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,5 +1,21 @@
 import './Footer.sass'
 
 export default function Footer(): JSX.Element {
-  return <footer />
+  return ( 
+    <footer>
+      <div className="footer-container">
+        <div className="footer-container__authors">
+          <h2>Authors:</h2>
+          <ul className="footer-container__authors__list">
+            <li><a href="https://github.com/uaolmer">React Developer</a></li>
+            <li><a href="https://github.com/uaolmer">React Developer</a></li>
+            <li><a href="https://github.com/uaolmer">React Developer</a></li>
+          </ul>
+        </div>
+        <div className="footer-container__copyright">
+          2021(c) RSSchool
+        </div>
+      </div>
+    </footer>
+  )
 }
--- client/src/components/HomePage/HomePage.tsx
+++ client/src/components/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import PageLogo from '../PokerPlanning/PokerPlanning'
 import Session from '../Session/Session'
+import "./HomePage.sass"
 
 export default function PageProducts(): JSX.Element {
   return (
