88295463b31af1b5467195962e2e1871e16d7ba6
iusmanof <iusmanof@gmail.com>
2021-09-09 23:54:24 +0300

    fix: eslint packagge config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,3 @@
-import './App.sass'
-import ScramMaster from './components/ScramMaster/scram-master'
-
 export default function App(): JSX.Element {
-  return (
-    <div className="App" > 
-      <ScramMaster linkLobby="google.com" />
-    </div>
-  )
-} 
+  return <div className="App" />
+}
