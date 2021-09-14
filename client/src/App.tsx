82dc4e3cfee88bf379d837292dfa5b8323e59f73
Vadim <vadimshut1992@gmail.com>
2021-09-14 14:15:18 +0300

    fix: fix header style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import './app.sass'
-// import ConnectToLobby from './components/ConnectToLobby/ConnectToLobby'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 import PageProducts from './components/HomePage/HomePage'
 
 export default function App(): JSX.Element {
   return (
     <div className="App">
+      <Header />
       <PageProducts />
-      {/* <ConnectToLobby /> */}
+      <Footer />
     </div>
   )
 }
