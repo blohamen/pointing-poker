69c130a42d9f5a403dcfca192fdddc443b228c3a
Vadim <vadimshut1992@gmail.com>
2021-09-16 16:14:39 +0300

    feat: implement redux toolkit, action whel click delete member

---
+++ client/src/components/MembersBlock/MembersBlock.tsx
@@ -0,0 +1,24 @@
+import { useAppSelector } from '../../store/redux'
+import MemberCard from '../Member-card'
+import './members-block.sass'
+
+const MembersBlock: React.FC = () => {
+  const isKickPlayer = useAppSelector((state) => state.isKick)
+  console.log('isKickPlayer: ', isKickPlayer)
+
+  return (
+    <div className="members-block">
+      <p className="members-block__title">Members</p>
+      <div className="members-block__members-wrapper">
+        <MemberCard title="Vadim Shut" subtitle="junior Front-End developer" photoURL="" isCancel isSmall />
+        <MemberCard title="Vadim Shut" subtitle="junior Front-End developer" photoURL="" isCancel isSmall />
+        <MemberCard title="Ihar Usmanov" subtitle="junior Front-End developer" photoURL="" isCancel isSmall />
+        <MemberCard title="Vadim Shut" subtitle="junior Front-End developer" photoURL="" isCancel isSmall />
+        <MemberCard title="Vadim Shut" subtitle="junior Front-End developer" photoURL="" isCancel isSmall />
+        <MemberCard title="Ihar Usmanov" subtitle="junior Front-End developer" photoURL="" isCancel isSmall />
+      </div>
+    </div>
+  )
+}
+
+export default MembersBlock
