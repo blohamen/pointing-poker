fdfabafd96bd4cd1986e48974fd41098d50792e2
iusmanof <iusmanof@gmail.com>
2021-10-01 06:46:36 +0300

    feat: add members in score sidebar

--- client/src/components/ScoreSidebar/ScoreSidebar.tsx
+++ client/src/components/ScoreSidebar/ScoreSidebar.tsx
@@ -1,30 +1,34 @@
 import React from 'react'
 import MemberCard from '../Member-card'
 import ProgressCard from '../ProgressCard'
+import IUser from '../../interfaces/IUser'
+import { useAppSelector } from '../../store/redux'
 
 import './ScoreSidebar.sass'
 
 const ScoreSidebar = () => {
-  return (
-    <div className="score-sidebar">
-      <div className="score-sidebar__progress-players">
-        <ProgressCard progress="2" />
-        <MemberCard title="Player 1" subtitle="subtitle" photoURL="test" socketId="id" isChatCard />
-      </div>
-      <div className="score-sidebar__progress-players">
-        <ProgressCard progress="In Progress" />
-        <MemberCard title="Player 1" subtitle="subtitle" photoURL="test" socketId="id" isChatCard />
-      </div>
-      <div className="score-sidebar__progress-players">
-        <ProgressCard progress="3" />
-        <MemberCard title="Player 1" subtitle="subtitle" photoURL="test" socketId="id" isChatCard />
-      </div>
-      <div className="score-sidebar__progress-players">
-        <ProgressCard progress="In Progress" />
-        <MemberCard title="Player 1" subtitle="subtitle" photoURL="test" socketId="id" isChatCard />
-      </div>
+  const { members } = useAppSelector((state) => state.membersParameters)
+  const { userId } = useAppSelector((state) => state.userParameters)
+  // const { scoreId, scoreValue } = useAppSelector((state) => state.scoreParameters)
+  // <ProgressCard progress={scoreValue} />
+
+  const membersWithScore = members.map((item: IUser) => (
+    <div className="score-sidebar__progress-players">
+      <ProgressCard progress="In Progress" />
+      <MemberCard
+        isCurrentPlayer={userId === item.userId}
+        title={`${item.firstName} ${item.lastName}`}
+        subtitle={item.jobPossition}
+        photoURL={item.image}
+        key={item.userId}
+        isCancel
+        isChatCard
+        socketId={item.socketId === undefined ? '' : item.socketId}
+      />
     </div>
-  )
+  ))
+
+  return <div className="score-sidebar">{membersWithScore}</div>
 }
 
 export default ScoreSidebar
