320480aef8512de121153839f8a05db0881d35b4
Vadim <vadimshut1992@gmail.com>
2021-10-01 15:13:36 +0300

    feat: implement tooltip to copy link button

--- client/src/components/LinkToLobby/LinkToLobby.tsx
+++ client/src/components/LinkToLobby/LinkToLobby.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppSelector } from '../../store/redux'
 import Button from '../Button/Button'
 import './link-to-lobby.sass'
@@ -9,22 +9,28 @@ interface ILinkToLobbyProps {
 
 const LinkToLobby: React.FC<ILinkToLobbyProps> = () => {
   const { roomId } = useAppSelector((state) => state.userParameters)
+  const [tooltipValue, setTooltipValue] = useState<string>('Copy to clipboard')
 
   const handleCopyLink = (event: React.SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault()
     const copyText: any = event.currentTarget.elements[0]
     window.navigator.clipboard.writeText(copyText.value)
+    setTooltipValue('room ID copied')
+    setTimeout(() => {
+      setTooltipValue('Copy to clipboard')
+    }, 5000)
   }
   return (
     <div className="scram">
       <h2 className="header-lobby">Link to Lobby</h2>
       <form className="linkLobby-block" onSubmit={handleCopyLink} id="link-form">
         <input className="text-input" type="text" disabled value={roomId} />
-        <Button value="Copy" size="small" theme="dark" form="link-form" />
-
-        <span className="tooltiptext" id="myTooltip">
-          Copy to clipboard
-        </span>
+        <div className="scram__btn-wrapper">
+          <Button value="Copy" size="small" theme="dark" form="link-form" />
+          <span className="tooltiptext" id="myTooltip">
+            {tooltipValue}
+          </span>
+        </div>
       </form>
     </div>
   )
