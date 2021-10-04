a4239871f0bf653364a2bb04f7797b295b394c77
iusmanof <iusmanof@gmail.com>
2021-10-04 14:59:57 +0300

    init: start statistic page

---
+++ client/src/views/StatisticPage/StatisticPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './StatisticPage.sass'
+
+const StatisticPage = () => {
+  return <div className="statistic-page">StatisticPage</div>
+}
+
+export default StatisticPage
